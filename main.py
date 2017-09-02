351a49df4a5cdb3099e337ebdc005c623684c6e9
tlentali <thomas.lentali@gmail.com>
2017-09-03 00:48:47 +0200

    understand where color are removed

--- main.py
+++ main.py
@@ -14,8 +14,15 @@ ret, label, center = cv2.kmeans(
     Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 # Now convert back into uint8, and make original image
-center = np.uint8(center)
+center = np.uint8(center)  # value off the color selected by algo
 res = center[label.flatten()]
+"""
+for i in range(len(res)):
+    if res[i][0] != 71:
+        res[i][0] = 0
+        res[i][1] = 0
+        res[i][2] = 0
+"""
 res2 = res.reshape((img.shape))
 
 cv2.imshow('res2', res2)
