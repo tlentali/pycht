3856ed8cfe0caad0672d7fd1245147266993ea3a
tlentali <thomas.lentali@gmail.com>
2017-09-22 11:47:47 +0200

    stencils separation by color  in comment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
+import pandas as pd
 
-img = cv2.imread('alice.jpg')
+img = cv2.imread('vincent.jpg')
 Z = img.reshape((-1, 3))
 
 # convert to np.float32
@@ -9,20 +10,40 @@ Z = np.float32(Z)
 
 # define criteria, number of clusters(K) and apply kmeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 2
+K = 8
 ret, label, center = cv2.kmeans(
     Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 # Now convert back into uint8, and make original image
 center = np.uint8(center)  # value off the color selected by algo
 res = center[label.flatten()]
+
+# separate differants colors
 """
-for i in range(len(res)):
-    if res[i][0] != 71:
-        res[i][0] = 0
-        res[i][1] = 0
-        res[i][2] = 0
+df = pd.DataFrame(res)
+df.columns = ['col1', 'col2', 'col3']
+df['tot'] = df["col1"].astype(
+    str) + df["col2"].astype(str) + df["col3"].astype(str)
+df['tot'].unique()
+
+for i in df['tot'].unique():
+    df_annexe = df
+    df_annexe["col1"].loc[(df['tot'] != i)] = 0
+    df_annexe["col2"].loc[(df['tot'] != i)] = 0
+    df_annexe["col3"].loc[(df['tot'] != i)] = 0
+    res_annexe = df_annexe[["col1", "col2", "col3"]].as_matrix()
 """
+
+"""    df_annexe = df
+    df_col1 = df_annexe.loc[~(df['tot'] != i]
+    df_col1["B"] = 0
+    df_annexe.loc[df_col1.index.tolist(), "B"]
+    df_annexe["col2"].loc[~(df['tot'] != i]=0
+    df_annexe["col3"].loc[~(df['tot'] != i]=0
+    res_annexe=df_annexe[["col1", "col2", "col3"]].as_matrix()
+"""
+# generate final image
+
 res2 = res.reshape((img.shape))
 
 cv2.imshow('res2', res2)
