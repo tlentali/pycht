e91625f0c630dfa01946e2399410e641e5c7f3fa
Thomas Lentali <tlentali@users.noreply.github.com>
2022-04-05 14:57:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
+
+# Package meta-data.
+NAME = 'cheshire'
+DESCRIPTION = 'Streetart by clustering.'
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+URL = 'https://github.com/tlentali/cheshire'
+EMAIL = 'thomas.lentali@gmail.com'
+AUTHOR = 'Thomas Lentali'
+REQUIRES_PYTHON = '>=3.6.0'
+LICENCE='MIT'
+VERSION = '0.1'
+
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='cheshire',
-    url='https://github.com/tlentali/cheshire',
-    author='Thomas Lentali',
-    author_email='thomas.lentali@gmail.com',
+    name=NAME,
+    url=URL,
+    author=AUTHOR,
+    author_email=EMAIL,
     # Needed to actually package something
     packages=['cheshire'],
     # Needed for dependencies
@@ -15,11 +27,19 @@ setup(
                       'python-dateutil==2.7.3',
                       'pytz==2018.5',
                       'six==1.11.0'],
-    # *strongly* suggested for sharing
-    version='0.1',
-    # The license can be anything you like
-    license='MIT',
-    description='From pics to stencil',
-    # We will also need a readme eventually (there will be a warning)
+    version=VERSION,
+    license=LICENCE,
+    description=DESCRIPTION,
     long_description=open('README.md').read(),
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+     keywords=['data-science', 'machine-learning', 'clustering','statistics', 'streetart', 'stencil', 'art'],
+    classifiers=[
+    # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
 )
