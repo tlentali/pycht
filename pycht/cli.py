7f6b936ceb690e5445dc8547b2b5c6a782983991
Thomas Lentali <tlentali@users.noreply.github.com>
2025-04-11 11:55:51 -0400

    Update pycht/cli.py with @garaud arguments improvement.
    Co-authored-by: Damien Garaud <damien.garaud@opendatasoft.com>

--- pycht/cli.py
+++ pycht/cli.py
@@ -3,15 +3,20 @@ Main project settings and execution logic.
 """
 
 import typer
+from typing_extensions import Annotated
 
 from .pycht import Pycht
 
-
-app = typer.Typer()
+app = typer.Typer(help="Main CLI for Pycht.")
 
 
 @app.command()
-def stencil(input_img: str, nb_colors: int = 4, output_path: str = "./"):
+def stencil(
+    input_img: Annotated[str, typer.Argument(help="The input image")],
+    nb_colors: Annotated[int, typer.Argument(help="Number of color clusters")] = 3,
+    output_path: Annotated[str, typer.Argument(help="The output folder")] = "./",
+):
+    """Stencil your picture with an input image 🎨 !"""
     typer.echo(f"Processing {input_img} into {output_path} with {nb_colors} levels.")
     Pycht().stencil(input_img, nb_colors, output_path)
 
